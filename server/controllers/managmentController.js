71f7edf1c32dd2d347bfd663ab89b35b8c9bec39
ALLIANCE-dieudonne <filalliance769@gmail.com>
2023-07-16 12:57:13 +0200

    add send

--- server/controllers/managmentController.js
+++ server/controllers/managmentController.js
@@ -5,7 +5,8 @@ export const getAdmins = async (req, res) => {
     const admins = await User.find({ role: "admin" }).select("-password");
     res.status(200).json(admins);
   } catch (error) {
-    res.status(404).json({ message: error.message });
+    // res.status(404).json({ message: error.message });
+    res.send(error.message);
   }
 };
 
